2903248871208fce1524fc3c8245603554c70293
ashhadwaleed <ashhadwaleed160@gmail.com>
2023-03-25 19:55:17 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingComponent } from './landing/landing.component';
+import { StarComponent } from './star/star.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { CopyrightComponent } from './copyright/copyright.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LandingComponent,
+    StarComponent,
+    PortfolioComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    CopyrightComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,19 @@
+import { HtmlTagDefinition } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.css']
+})
+
+export class PortfolioComponent {
+
+}
+
+
+
+
+
+
+
